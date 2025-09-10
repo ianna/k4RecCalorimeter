36c9ddebd8727fd7f15a0efbca1124d05cd0f769
Ianna Osborne <ianna.osborne@cern.ch>
2025-09-10 13:36:55 +0200

    modify config file

--- RecCalorimeter/src/components/digi_dualcrys.py
+++ RecCalorimeter/src/components/digi_dualcrys.py
@@ -5,9 +5,9 @@ digi.calCollections = "SimCaloHits"
 digi.outputCalCollection = "DigitizedCaloHits"
 digi.outputRelCollection = "CaloHitLinks"
 digi.CalThreshold = 0.03  # MeV
-digi.calibrationCoeffcal = 120000.0
+# digi.calibrationCoeffcal = 120000.0
 digi.maxCalHitEnergy = 2.0
-digi.detectornameEcal = "DRCrystal"
-digi.detectornameHcal = "DRFtubeFiber"
+# digi.detectornameEcal = "DRCrystal"
+# digi.detectornameHcal = "DRFtubeFiber"
 
 # Then include this in your ApplicationMgr.TopAlg or ComponentAccumulator if you're using GaudiConfig2
